bd591204184a1b82a60608440431eabc98e22fad
viniciusdnb <viniciusdnb@hotmail.com>
2026-02-23 16:07:48 -0300

    criando controller da fila de gravacao

--- app/controller/lancamento/filaSerigrafiaController.js
+++ app/controller/lancamento/filaSerigrafiaController.js
@@ -2,53 +2,120 @@
 const ordemProducaoModel = require('../../model/models/lancamento/ordemProducaoModel');
 const clienteModel = require('../../model/models/cadastro/clienteModel');
 const produtoModel = require('../../model/models/cadastro/produtoModel');
-const list_apont_sum_qtd_grop_idOpModelView = require('../../model/models/lancamento/list_apont_sum_qtd_grop_idOpModelView');
-const { Op, fn, col } = require('sequelize');
+const list_apont_sum_qtd_grop_idOp = require('../../model/models/lancamento/list_apont_sum_qtd_grop_idOpModelView');
+const { Op, or } = require('sequelize');
+const maquinaModel = require('../../model/models/cadastro/maquinaModel');
+const fornoModel = require('../../model/models/cadastro/fornoModel');
+const filaMaquina = require('../../model/models/lancamento/fila_maquinaModel');
+const filaGravacaoModel = require('../../model/models/lancamento/fila_gravacaoModel');
 
 ordemProducaoModel.belongsTo(clienteModel, { foreignKey: "idCliente" });
 ordemProducaoModel.belongsTo(produtoModel, { foreignKey: "idProduto" });
-list_apont_sum_qtd_grop_idOpModelView.belongsTo(ordemProducaoModel, { foreignKey: "idOrdemProducao" });
+
 
 module.exports = {
     index: async function (req, res) {
-        let list = await list_apont_sum_qtd_grop_idOpModelView.findAll(
+        let ordemProducao = await ordemProducaoModel.findAll(
             {
-                
-                attributes:[
-                    'idOrdemProducao',
-                    [fn('sum', col('list_apont_sum_qtd_grop_idOp.quantidade')),'total']
-                ]
-                ,
-                where: { idSetor: 2 },
-                
-                group: "idOrdemProducao",
-                include: [
-                    {
-                        model: ordemProducaoModel,
-                        where: {
-                            [Op.or]: [
-                                { idStatus: 5 },
-                                { idStatus: 6 },
-
-                            ]
-                        },
-                        
-                        
-                        include: [
-                            { model: clienteModel },
-                            { model: produtoModel }
-                        ]
-                    },
-
-                ]
-
+                where: { idStatus: { [Op.or]: [5, 6] } },
+                include: [clienteModel, produtoModel]
             }
-        )
+        );
+        let arrOrdemProducao = JSON.parse(JSON.stringify(ordemProducao, null));
+        //funcao para percorrer as ordens de producao e verificar se elas tem lancamentos nos setor da gravacao 
+        for (const key in arrOrdemProducao) {
+            let ordem = arrOrdemProducao[key]
+            let list = await list_apont_sum_qtd_grop_idOp.findAll({ where: { idOrdemProducao: ordem.idOrdemProducao, idSetor: 2 } });
+            let sumQuantidade = 0
+            if (list.length != 0) {
+                let arrList = JSON.parse(JSON.stringify(list, null));
+                arrList.forEach(elementList => {
+                    sumQuantidade += Number(elementList.quantidade)
+                });
+            }
 
-        console.log(JSON.parse(JSON.stringify(list, null)));
+            arrOrdemProducao[key]["quantidadeProduzida"] = sumQuantidade
+
+        }
+
+
         res.render('lancamento/filaserigrafia', {
-            'data': JSON.parse(JSON.stringify(list, null)),
+            'data': arrOrdemProducao,
             'pathName': 'main'
         });
+    },
+    setup: async function (req, res) {
+        let idOrdemProducao = req.params.idOrdemProducao
+        let ordemProducao = await ordemProducaoModel.findAll({
+            where: { idOrdemProducao: idOrdemProducao },
+            include: [clienteModel, produtoModel]
+        });
+        
+        let fornos = await fornoModel.findAll();
+
+        let arrOrdemProducao = JSON.parse(JSON.stringify(ordemProducao, null));
+       
+        let arrFornos = JSON.parse(JSON.stringify(fornos, null));
+
+        
+
+       
+        res.render('lancamento/filaserigrafia', {
+            'arrOrdemProducao': arrOrdemProducao,
+            'arrMaquinas': await this.verificaMaquinaDisponivel(idOrdemProducao),
+            'arrFornos': arrFornos,
+            'pathName': 'setup'
+        });
+    },
+    getfilaMaquinas: async function(idOrdemProducao){
+        let filaGravacao = await filaGravacaoModel.findAll({where:{idOrdemProducao:idOrdemProducao}});
+        let arrFilaGravacao = JSON.parse(JSON.stringify(filaGravacao, null));
+        let arrMaquinas = [];
+        let key = 0;
+        arrFilaGravacao.forEach(maquinas=>{
+            arrMaquinas[key] = {"idMaquina": maquinas.idMaquina}
+            key++;
+        });
+
+        return arrMaquinas;
+    },
+
+    verificaMaquinaDisponivel: async function(idOrdemProducao)
+    {
+        let arrMaquinasEmUso = this.getfilaMaquinas(idOrdemProducao);
+        let maquinas = await maquinaModel.findAll({ where: { idSetor: 2 } });
+        let arrMaquinas = JSON.parse(JSON.stringify(maquinas, null));
+        let arrMaquinasDisponivel = [];
+        let key = 0;
+        arrMaquinas.forEach(maquina =>{
+            if(!Object.values(maquina).includes(arrMaquinasEmUso)){
+                arrMaquinasDisponivel[key]=maquina;
+            }
+            key++;
+        });
+
+        return arrMaquinasDisponivel;
+    },
+    insert: async function(req, res){
+       
+
+
+       
+    },
+
+    verificaMaquinaFornoOp: async function(req){
+        let arrMaquinas = req.body.maquinas;
+        let forno = req.body.forno;
+        let idOrdemProducao = req.body.idMaquina;
+
+        let filaGravacao = await filaGravacaoModel.findAll({
+            where:{
+                [Op.and]:[
+                    {}
+                ]
+            }
+        })
     }
+
+
 }

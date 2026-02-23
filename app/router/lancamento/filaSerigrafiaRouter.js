413ee88ebaa1a264541ae6d797c84477d8ee1317
viniciusdnb <viniciusdnb@hotmail.com>
2026-02-23 16:07:13 -0300

    criando rota da fila de gravacao

--- app/router/lancamento/filaSerigrafiaRouter.js
+++ app/router/lancamento/filaSerigrafiaRouter.js
@@ -4,6 +4,14 @@ const filaSerigrafiaController = require('../../controller/lancamento/filaSerigr
 
 filaSerigrafiaRouter.get('/fila-serigrafia', function(req, res){
     filaSerigrafiaController.index(req, res);
+});
+
+filaSerigrafiaRouter.get('/fila-setup/:idOrdemProducao', function(req, res){
+    filaSerigrafiaController.setup(req, res);
+});
+
+filaSerigrafiaRouter.post('/fila-serigrafia', function(req, res){
+    filaSerigrafiaController.insert(req, res);
 })
 
 module.exports = filaSerigrafiaRouter;
